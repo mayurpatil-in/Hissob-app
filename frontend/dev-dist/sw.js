d0201af33fe34257ed422db2acf31bdeb1792230
mayurpatil-in <ai.mayurpatil@gmail.com>
2026-08-02 13:57:40 +0530

    update

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-0d539898'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ruddjqbf6s4"
+    "revision": "0.onb7qpacqdk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
